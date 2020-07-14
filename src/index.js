88c5495ad8e67945853874521c89d5137f7885c4
Daniela Aparecida Amadeu de Proença <amadev@amadev.localhost>
2020-07-14 11:28:53 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useContext, useReducer, useState, useEffect } from "react"
+import ReactDOM from "react-dom"
+import axios from "axios"
+import TodosContext from "./context"
+import todosReducer from "./reducer"
+import TodoList from "./components/TodoList"
+import TodoForm from "./components/TodoForm"
+const useAPI = (endpoint) => {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    getData()
+  }, [])
+  const getData = async () => {
+    const response = await axios.get(endpoint)
+    setData(response.data)
+  }
+  return data
+}
+
+const App = () => {
+  const initialState = useContext(TodosContext)
+  const [state, dispatch] = useReducer(todosReducer, initialState)
+  const savedTodos = useAPI("https://hooks-api-master-seven.vercel.app/todos")
+
+  useEffect(() => {
+    dispatch({
+      type: "GET_TODOS",
+      payload: savedTodos,
+    })
+  }, [savedTodos])
+
+  return (
+    <TodosContext.Provider value={{ state, dispatch }}>
+      <TodoForm />
+      <TodoList />
+    </TodosContext.Provider>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  document.getElementById("root")
+)
